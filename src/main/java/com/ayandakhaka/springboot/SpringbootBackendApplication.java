66609668c65bf03bf03dde339ae2a1b0537d244e
Ayanda Khaka <khakalihle@gmail.com>
2025-08-19 15:23:14 +0200

    fixing pipeline failure due war file not found

--- src/main/java/com/ayandakhaka/springboot/SpringbootBackendApplication.java
+++ src/main/java/com/ayandakhaka/springboot/SpringbootBackendApplication.java
@@ -2,6 +2,7 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ui.ModelMap;
 
@@ -16,5 +17,10 @@ public class SpringbootBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
+	
+	// This makes it deployable as a WAR to Tomcat
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootBackendApplication.class);
+    }
 
 }
